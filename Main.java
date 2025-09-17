938f401ff71f334f034ad586a7620d1709b3a7bd
Joey Chen <joeyc43@nycstudents.net>
2025-09-17 15:32:31 -0400

    added test cases

--- Main.java
+++ Main.java
@@ -9,10 +9,14 @@ public class Main
         System.out.println("Current Food: " + f.getCurrentFood());
         f = new Feeder (1000);
         f.simulateOneDay(22);
+        f = new Feeder (100);
+        f.simulateOneDay(5);
         System.out.println("Current Food: " + f.getCurrentFood());
         f = new Feeder(2400);
         System.out.println(f.simulateManyDays(10,4) + " day(s)");
         f = new Feeder(250);
         System.out.println(f.simulateManyDays(10,5) + " day(s)");
+        f = new Feeder(0);
+        System.out.println(f.simulateManyDays(5,10) + " day(s)");
     }
 }
